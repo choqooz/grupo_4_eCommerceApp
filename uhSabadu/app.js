d5311a92907b8bc6d912198ce23bc6f8e36c4953
Sergio Fernandez <sergiojujuy2012@gmail.com>
2024-01-06 18:20:05 -0300

    agregada la estructura de las vistas

---
+++ uhSabadu/app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+const publicPath = path.join(__dirname, '/public')
+app.use(express.static(publicPath))
+
+app.get('/', (req, res)=>{
+    let homePath = path.join(__dirname, 'views', 'home.html')
+    res.sendFile(homePath)
+})
+
+app.get('/register', (req, res)=>{
+    let homePath = path.join(__dirname, 'views', 'register.html')
+    res.sendFile(homePath)
+})
+
+const port = 3737
+app.listen(port, ()=>{
+    console.log(`El servidor esta corriendo en http://localhost:${port} 🚀🚀🚀`);
+   
+})
